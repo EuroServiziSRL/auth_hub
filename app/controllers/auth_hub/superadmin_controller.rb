66e9f68c2e821a2433c5e355ed9500eccc01c4f6
fabianopavan <fabianopavan84@gmail.com>
2018-07-11 07:13:31 +0000

    Salvo in sessione i link per la navigazione del super user

--- app/controllers/auth_hub/superadmin_controller.rb
+++ app/controllers/auth_hub/superadmin_controller.rb
@@ -2,11 +2,22 @@ require_dependency "auth_hub/application_controller"
 
 module AuthHub
   class SuperadminController < ApplicationController
+    before_action :valorizza_link_navigazione
     
     #view con vari link
     def index
       @nome_pagina = "Home"
     end
+  
+  
+  
+  
+    def valorizza_link_navigazione
+      session[:link_navigazione_utente] = []
+      session[:link_navigazione_utente] << {'titolo' => 'Lista Utenti', 'url' => users_path }
+      session[:link_navigazione_utente] << {'titolo' => 'Utenti Da Confermare', 'url' => utenti_da_confermare_path }
+    end
+  
   
   end
 end
