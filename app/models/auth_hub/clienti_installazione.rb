07976db72635e6ac15435b14d8ccc24dfadf1700
fabiano <fabiano.pavan@soluzionipa.it>
2020-03-24 12:02:33 +0100

    Controllo che campi SPIDERURL E SPIDERDB non siano stringhe vuote per caricare installazioni spider

--- app/models/auth_hub/clienti_installazione.rb
+++ app/models/auth_hub/clienti_installazione.rb
@@ -6,7 +6,7 @@ module AuthHub
         has_many :clienti_applinstallate, class_name: 'AuthHub::ClientiApplinstallate', :foreign_key => "ID_INSTALLAZIONE"
         belongs_to :clienti_cliente, class_name: 'AuthHub::ClientiCliente'#, :foreign_key => "ID_ANAGRAFICA"
     
-        scope :installazione_ruby, ->(id_cliente){ where(ID_ANAGRAFICA: id_cliente).where("SPIDERDB IS NOT NULL AND SPIDERURL IS NOT NULL") }
+        scope :installazione_ruby, ->(id_cliente){ where(ID_ANAGRAFICA: id_cliente).where("SPIDERDB IS NOT NULL AND SPIDERDB <> '' AND SPIDERURL IS NOT NULL AND SPIDERURL <> ''") }
     
         def installazione_ruby?
             return !self.SPIDERDB.blank? && self.HIPPO.blank? && !self.SPIDERURL.blank?
