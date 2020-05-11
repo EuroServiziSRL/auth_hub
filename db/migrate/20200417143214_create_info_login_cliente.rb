39c0cf2559e1a619aa30eb31d56c1b2565e7124b
fabiano <fabiano.pavan@soluzionipa.it>
2020-05-11 11:18:52 +0200

    Cambiato ordine campi per vedere subito info su info clienti login

--- db/migrate/20200417143214_create_info_login_cliente.rb
+++ db/migrate/20200417143214_create_info_login_cliente.rb
@@ -1,6 +1,9 @@
 class CreateInfoLoginCliente < ActiveRecord::Migration[5.2]
   def change
     create_table :auth_hub_info_login_cliente do |t|
+      t.string  :org_name,            null: false
+      t.string  :org_display_name,    null: false
+      t.string  :org_url,             null: false
       t.string  :key_path,            null: false
       t.string  :cert_path,           null: false
       t.boolean :app_ext,             null: true
@@ -9,9 +12,6 @@ class CreateInfoLoginCliente < ActiveRecord::Migration[5.2]
       t.string  :url_app_ext,         null: true
       t.string  :url_ass_cons_ext,    null: true
       t.string  :issuer,              null: false
-      t.string  :org_name,            null: false
-      t.string  :org_display_name,    null: false
-      t.string  :org_url,             null: false
       t.boolean :spid,                null: true
       t.boolean :spid_pre_prod,       null: true
       t.boolean :cie,                 null: true
