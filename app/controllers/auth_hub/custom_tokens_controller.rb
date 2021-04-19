a8a79b5e2d878579d3181acc205eda1ee6682097
fabiano <fabiano.pavan@soluzionipa.it>
2021-04-19 14:59:54 +0200

    Fix errori per user e su custom_tokens_controller per gestione eccezione

--- app/controllers/auth_hub/custom_tokens_controller.rb
+++ app/controllers/auth_hub/custom_tokens_controller.rb
@@ -22,7 +22,7 @@ module AuthHub
             headers.merge!(authorize_response.headers)
             render json: authorize_response.body,
                 status: authorize_response.status
-        rescue Errors::DoorkeeperError => e
+        rescue ::Doorkeeper::Errors::DoorkeeperError => e
             handle_token_exception(e)
         end
 
